133f8c459a091653d3c1d398cc29a3a4e3c59bef
Yhorman Bedoya <yh.bv.0829@gmail.com>
2022-08-24 22:26:41 +0200

    init commit

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -59,7 +59,7 @@ def train_one_epoch(model: torch.nn.Module,
         if (data_iter_step + 1) % accum_iter == 0:
             optimizer.zero_grad()
 
-        #torch.cuda.synchronize()
+        torch.cuda.synchronize()
 
         metric_logger.update(loss=loss_value)
 
