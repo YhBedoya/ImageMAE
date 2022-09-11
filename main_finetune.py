e820f72d75159b11844f01899fd4657080f63bf1
Yhorman Bedoya <yh.bv.0829@gmail.com>
2022-09-11 18:55:41 +0200

    init commit

--- main_finetune.py
+++ main_finetune.py
@@ -224,7 +224,7 @@ def main(args):
             prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
             label_smoothing=args.smoothing, num_classes=args.nb_classes)
 
-    model = models_vit.__dict__[args.model](
+    model = models_mae.__dict__[args.model](
         num_classes=args.nb_classes,
         drop_path_rate=args.drop_path,
         global_pool=args.global_pool,
